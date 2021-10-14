b41d741ea27fafa61abae4bf27e009019594d56d
Daniel Jeller <djeller@nettek.at>
2021-10-14 22:21:08 +0200

    Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,37 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
   output: [
     {
-    file: pkg.main,
-    format: "cjs",
-    sourcemap: true,
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+      name: "use-nampi-api",
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+      sourcemap: true,
     },
     //  {
-      //  file: pkg.module,
-      //  format: "es",
-      //  sourcemap: true,
-    //  },
-    //  {
-      //  file: pkg.browser,
-      //  format: "iife",
-      //  name: "UseNampiApi",
-      //  sourcemap: true,
+    //  file: pkg.browser,
+    //  format: "iife",
+    //  name: "UseNampiApi",
+    //  sourcemap: true,
     //  },
   ],
   //  external: [...Object.keys(pkg.dependencies || {})],
-  plugins: [peerDepsExternal(), typescript(), terser()],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    terser(),
+  ],
 };
