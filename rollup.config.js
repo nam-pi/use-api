ba7c66b433dc857c587ef9aefc53e0a5d976c47c
Daniel Jeller <djeller@nettek.at>
2021-10-14 17:36:45 +0200

    Change build setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
-import ts from "rollup-plugin-ts";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
-  output: {
-    file: "dist/bundle.js",
-    format: "cjs",
-    // name: "main"
-    sourcemap: true,
-  },
-  plugins: [ts()],
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [typescript()],
+  external: ["react", "react-dom"],
 };
